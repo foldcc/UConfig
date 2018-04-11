4c8c07ffab469479ef5b44c8232f27eab6b86746
ZHBSFoldcc <foldcc@zhbs.com>
2018-04-11 16:59:30 +0800

    增加config构建器 增加config读取工具类

---
+++ Assets/Demo/scripts/DemoConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DemoConfig : ScriptableObject {
+
+    public string userName;
+
+    public int userID;
+
+    public void print() {
+        Debug.Log(string.Format("name: {0} id: {1}" , userName , userID));
+    }
+}
---
+++ Assets/UConfig/Scripts/UConfigHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ namespace UConfig {
+    public static class UConfigHelper
+    {
+        /// <summary>
+        /// 将指定的配置文件转化为指定对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="script"></param>
+        /// <returns></returns>
+        public static T GetObjectWithUConfig<T>(ScriptableObject script) where T : ScriptableObject {
+            return script as T;
+        }
+    }
+}
+
+
